2e3ef675c079d56e702fbab061cd22c12c528ea4
fitse966 <eyerusalemdegif9@gmail.com>
2025-11-25 14:46:10 +0300

    Fix CORS for Netlify

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,16 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://musical-gaufre-7f3289.netlify.app",
+      "https://twist-hair-backend.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
